8c1a5f113e0c5b35edb2ce7fdd8449a251a32463
alextwilson <github@alextwilson.co.uk>
2018-02-01 12:01:14 +0000

    temperature is increased by 1 when up is called

---
+++ jasmine/spec/thermostatSpec.js
@@ -0,0 +1,21 @@
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat()
+  });
+
+  describe('temperature', function() {
+    it('starts at 20', function() {
+      expect(thermostat.temperature).toEqual(20)
+    });
+  });
+
+  describe('changing temperature', function() {
+    it('increases by one when up is called', function() {
+      thermostat.up()
+      expect(thermostat.temperature).toEqual(21)
+    });
+  });
+});
---
+++ jasmine/src/thermostat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature++
+};
