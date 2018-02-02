bd1c85a49002b226079bbc6b56a87f9ad00fc3af
alextwilson <github@alextwilson.co.uk>
2018-02-02 15:19:50 +0000

    Has local city weather

--- interface.js
+++ interface.js
@@ -27,6 +27,15 @@ $(document).ready(function() {
     updateTemperature();
   });
 
+  $('#enter-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=68d2a851cc0a640f583329890053e9f7', function(data) {
+      $('#local_temperature').text(data.main.temp)
+      $('#conditions').text(data.weather[0].description)
+    });
+  });
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
